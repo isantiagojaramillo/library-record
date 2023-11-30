952d275a461e9e9133380279863fcb3b7dbec137
isantiagojaramillo <sajaramillogr@cesde.net>
2023-11-30 15:51:12 -0500

    updated

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,12 @@
 <?php
+    // $server = "localhost";
+    // $database = "library";
     $server = "localhost";
-    $database = "library";
+    $database = "id21601161_form";
 
-    $user = "root";
+    // $user = "root";
+    // $password = "";
+    $user = "id21601161_root";
     $password = "";
 
     $connection = new PDO("mysql:server=$server;port=3307;dbname=$database", "$user", "$password");
